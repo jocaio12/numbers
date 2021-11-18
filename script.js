a360f8416904bba7f0908482a33d40bc7176cc4a
Joao Caio <jocaio@uol.com.br>
2021-11-18 19:00:21 -0300

    Aulas B7W Numbers

---
+++ script.js
@@ -0,0 +1,38 @@
+let n = '25.9';
+
+let res = parseFloat(n) + 5;
+
+
+console.log(res);
+
+/*
+// transforma a string com decimal em numero com decimal
+let n = '25.9';
+
+let res = parseFloat(n) + 5;
+*/
+
+
+/*
+// transforma a string em numero
+let n = '25';
+
+let res = parseInt(n) + 5;
+*/
+
+
+
+/*
+// pega somente o numero de casas decimais determinadas
+let n = 10.4588945689;
+
+let res = n.toFixed(3);
+*/
+
+
+/*
+// trasnforma o numero em string
+let n = 10;
+
+let res = n.toString() + 5;
+*/
